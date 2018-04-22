631403a94def7823121a7087667c25468013a6ba
Gaurav Koley <arkokoley@live.in>
2018-04-22 22:30:32 +0530

    devise working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
-	include ActionController::MimeResponds
+  include ActionController::Helpers
+  include ActionController::Flash
+  include ActionController::MimeResponds
 	respond_to :json
 end
