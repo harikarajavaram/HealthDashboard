8daffcd59edaff9a79c5f81101975405fad9ac51
Gaurav Koley <arkokoley@live.in>
2018-04-22 21:31:43 +0530

    merging

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#JSON.parse(File.read("heartrate_try.json")).each do |h|
-#	puts h
-#	Heartrate.create time: Time.at(h['x']/1000), value: h['y'], patient_id: 1
-#end
-
 JSON.parse(File.read("events.json")).each do |e|
 	puts e
 	Event.create name: e['activityName'], 
@@ -17,4 +12,12 @@ JSON.parse(File.read("events.json")).each do |e|
 				calories: e['calories'], 
 				start_time: DateTime.parse(e['startTimeGMT'])	, 
 				event_type: e['eventType']['typeId']
+end
+JSON.parse(File.read("heartrate_try.json")).each do |h|
+	puts h
+	Heartrate.create time: Time.at(h['x']/1000), value: h['y'], patient_id: 1
+end
+JSON.parse(File.read("16-03-2018.json"))['heartRateValues'].each do |h|
+	puts h
+	Heartrate.create time: Time.at(h[0]/1000), value: h[1], patient_id: 1
 end
